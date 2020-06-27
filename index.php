80dce1dbbfdedd410ad519cfa879d2660f856b23
Roy Timmermans <info@roytimmermans.be>
2020-06-27 11:34:31 +0200

    Project Start
    All initial Files

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<div>
+<h2>Generate a list of MOD IDs from a Steam collection.</h1>
+<form action="mods.php" method="post"> 
+<label for="link">Provide your Steam Collection Share Link below:</label>
+<input type="text" id="link" name="link" placeholder="Steam Collection Share Link.."><br>
+<input type="submit" value="Generate Mod IDs">
+</form>
+<h2>How to get your Collection Share Link?</h2>
+<img src="collection.png" class="center"/>
+<img src="share.png" class="center"/>
+</div>
+</body>
+</html>
---
+++ mods.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<div>
+<h2>Below are your mod IDs</h2>
+<?php
+$input = $_POST["link"];
+$file = file_get_contents($input);
+$muster = '<a href="https:\/\/steamcommunity.com\/sharedfiles\/filedetails\/\?id=([0-9]+)">';
+
+preg_match_all($muster, $file, $match);
+
+$arr = array_unique(array_values($match[1]));
+sort($arr);
+
+foreach($arr as $val) {
+	print $val . "<br/>";
+}
+
+?>
+</div>
+</body>
+</html>
